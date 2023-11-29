45c9fe227f836bccff41403b585f70fc65a2ebd0
iiftikar26-gcds <135147330+iiftikar26-gcds@users.noreply.github.com>
2023-11-29 12:32:09 -0500

    Add files via upload

--- IrhanIftikarFileIO.py
+++ IrhanIftikarFileIO.py
@@ -1,6 +1,7 @@
 #Name: Irhan Iftikar
 #Date: November 2023
 #Description: .csv file-reading program that reads from a database of GCDS students and allows user to make menu choices
+#Criteria: Meets spec criteria #1-12 (inclusive), and #14
 #Challenges: Creates graphs from data, deletes records from .csv, updates and changes records to .csv, creates a menu, uses functions, program repeats
 #Bugs: No notable bugs found in program
 #Sources: Various Internet Syntax Sources (w3schools, Stack Overflow, GeeksForGeeks, etc.)
@@ -11,9 +12,11 @@ from csv import writer
 import matplotlib.pyplot as plt
 import pandas as pd    
 
-#Function that outputs the main screen menu options to the user
 def main_screen():
-        print('''
+    #Description: Function that outputs the main screen menu options to the user
+    #parameters - void
+    #returns - void
+    print('''
     GCDS Database File Menu:
     -------------------------------------------------------------
     1 - Add Data Record
@@ -27,22 +30,26 @@ def main_screen():
     9 - Sort & Graph Data Records by 6th Grade Males Living in Greenwich
     10 - Quit''')  
 
-#Function that adds a new data record to .csv file
 def add():
+    #Description: Function that adds a new data record to .csv file
+    #parameters - void
+    #returns - void
     new_entry = []
     first_name, middle_name, last_name, grade, gender, teacher, city, state, zip_code = input("Enter first name, middle, last, grade, gender, teacher, city, \nstate, and zip code (separated by commas, capitalized, no spacing): ").split(",", 9)
     new_entry.extend((first_name, middle_name, last_name, grade, gender, teacher, city, state, zip_code))
     with open("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv", "a", newline='') as fp:
         writer_object = writer(fp)
-        writer_object.writerow(new_entry)
+        writer_object.writerow(new_entry)   #Writes new user entry data to existing .csv
         print("Data record added!")
 
-#Function that takes in the opened .csv file and updates an existing data record to the .csv file
 def update(file_in):
-    output = False
+    #Description: Function that takes in the opened .csv file and updates an existing data record to the .csv file
+    #parameters - file_in
+    #returns - void
+    output = False        #Boolean used to verify if requested data is found
     df = pd.read_csv(file_in)       #Using Pandas to save the inputted .csv file to a dataframe called df
     user_search = input("Enter the FULL name of the student to update (case sensitive): ")
-    index_count = -2
+    index_count = -2      #Starts index count at -2 to account for pandas formatting
     for line in open("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv"):
         list_of_words = line.split(",")
         index_count += 1
@@ -50,6 +57,7 @@ def update(file_in):
             output = True
             print("Data record found!")
             first_name, middle_name, last_name, grade, gender, teacher, city, state, zip_code = input("Enter updated first name, middle, last, grade, gender, teacher, city, \nstate, and zip code (separated by commas, capitalized, no spacing): ").split(",", 9)
+            
             #Code below uses Pandas to replace the appropriate index row with the updated information given by the user to the .csv file
             df.loc[index_count] = [first_name, middle_name, last_name, grade, gender, teacher, city, state, zip_code]
             df.to_csv("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv", index=False)
@@ -58,17 +66,20 @@ def update(file_in):
     if output == False:     #If requested data record isn't found
         print("Data record not found.")
 
-#Function that takes in the opened .csv file and deletes an existing data record and saves to the .csv file
 def delete(file_in):
-    output = False
+    #Description: Function that takes in the opened .csv file and deletes an existing data record and saves to the .csv file
+    #parameters - file_in
+    #returns - void
+    output = False                  #Boolean used to verify if requested data is found
     df = pd.read_csv(file_in)      #Using Pandas to save the inputted .csv file to a dataframe called df
     user_search = input("Enter the FULL name of the student to delete (case sensitive): ")
-    index_count = -2
+    index_count = -2        #Starts index count at -2 to account for pandas formatting
     for line in open("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv"):
         list_of_words = line.split(",")
         index_count += 1
         if (list_of_words[0] + " " + list_of_words[2]) == user_search:
             output = True
+
             #Code below uses Pandas to delete the index row of the record the user wants to delete in the .csv file
             df = df.drop(index_count)
             df.to_csv("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv", index=False)
@@ -77,9 +88,11 @@ def delete(file_in):
     if output == False:
         print("Data record not found.")
 
-#Function that takes in the opened .csv file and searches for a data record by first or last name. Outputs all the information of the record if found.
 def search(file_in):
-    output = False
+    #Description: Function that takes in the opened .csv file and searches for a data record by first or last name. Outputs all the information of the record if found.
+    #parameters - file_in
+    #returns - void
+    output = False          #Boolean used to verify if requested data is found
     search = input("Enter EITHER the first or last name of the student to find (case sensitive): ")
     for line in file_in:
         list_of_words = line.split(",") 
@@ -91,16 +104,20 @@ def search(file_in):
     if output == False:
         print("Data record not found.")
 
-#Function that takes in the opened .csv file and outputs listed data records alphabetically by last name
-#Note: Pandas lists last names starting with a lowercase letter such as 'deVeer' or 'van Niekerk' at the end as it sees these as different string types alphabetically 
-#Other than the exceptions with the lowercase last names, the function properly prints the last names alphabetically
 def list_name(file_in):
+    #Description: Function that takes in the opened .csv file and outputs listed data records alphabetically by last name
+    #parameters - file_in
+    #returns - void
+    #Note: Pandas lists last names starting with a lowercase letter such as 'deVeer' or 'van Niekerk' at the end as it sees these as different string types alphabetically 
+    #Other than the exceptions with the lowercase last names, the function properly prints the last names alphabetically
     df = pd.read_csv(file_in)
     df = df.sort_values(by=['NameLast', 'NameFirst'], ascending=True)
     print(df)
 
-#Function that takes in opened .csv file and outputs sorted list and graph of proportion of students in grades 9-12
 def sort_grade(file_in):
+    #Description: Function that takes in opened .csv file and outputs sorted list and graph of proportion of students in grades 9-12
+    #parameters - file_in
+    #returns - void
     seniors = 0
     juniors = 0
     sophomores = 0
@@ -129,8 +146,10 @@ def sort_grade(file_in):
     plt.ylabel('Number of Students')
     plt.show()
 
-#Function that takes in opened .csv file and outputs a sorted list and graph of proportion of students by gender
 def sort_gender(file_in):
+    #Description: Function that takes in opened .csv file and outputs a sorted list and graph of proportion of students by gender
+    #parameters - file_in
+    #returns - void
     boys = 0
     girls = 0
     for line in file_in:   
@@ -151,8 +170,10 @@ def sort_gender(file_in):
     plt.ylabel('Number of Students')
     plt.show()
 
-#Function that takes in opened .csv file and outputs a sorted list and graph of proportion of students by state
 def sort_state(file_in):
+    #Description: Function that takes in opened .csv file and outputs a sorted list and graph of proportion of students by state
+    #parameters - file_in
+    #returns - void
     connecticut = 0
     new_york = 0
     for line in file_in:   
@@ -173,8 +194,10 @@ def sort_state(file_in):
     plt.ylabel('Number of Students')
     plt.show()
     
-#Function that takes in opened .csv file and outputs a sorted list and graph of proportion of 6th Grade Males in Greenwich
 def gradesix_males_greenwich(file_in):
+    #Description: Function that takes in opened .csv file and outputs a sorted list and graph of proportion of 6th Grade Males in Greenwich
+    #parameters - file_in
+    #returns - void
     gradesix_males_greenwich = 0
     gradesix = 0
     for line in file_in:   
@@ -194,12 +217,16 @@ def gradesix_males_greenwich(file_in):
     plt.ylabel('Number of Students')
     plt.show()
 
-#Function that terminates the program
 def quit():
+    #Description: Function that terminates the program
+    #parameters - void
+    #returns - void
     sys.exit(0)
 
-#Main function that prompts user for input and executes the other functions
 def main():
+    #Description: Main function that prompts user for input and executes the other functions
+    #parameters - void
+    #returns - void
     file_in = open("C:\\Users\\iiftikar26\\Desktop\\GCDS Comp Sci\\gcds_data.csv")
     choice = input("Select an option from the menu (numbers 1-10): ")
     if choice == "1":
